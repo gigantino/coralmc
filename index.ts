94ace822350a0b0d30614d230ca0b0d42ab93878
gigantino <git@gigantino.dev>
2023-10-07 00:17:39 +0200

    feat: change formatting for bedwars and kitpvp to avoid empty res

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ function isUsernameValid(username: string) {
 }
 
 type PlayerStats = {
-  bedwars: {
+  bedwars?: {
     level: number;
     experience: number;
     coins: number;
@@ -19,7 +19,7 @@ type PlayerStats = {
     winstreak: number;
     highestWinstreak: number;
   };
-  kitpvp: {
+  kitpvp?: {
     balance: number;
     kills: number;
     deaths: number;
@@ -31,7 +31,7 @@ type PlayerStats = {
 };
 
 export async function getPlayerStats(
-  username: string
+  username: string,
 ): Promise<PlayerStats | undefined> {
   if (!isUsernameValid(username)) return undefined;
   const data = await fetch(`https://api.coralmc.it/api/user/${username}`);
@@ -39,32 +39,35 @@ export async function getPlayerStats(
 
   const { bedwars, kitpvp } = json;
 
-  if (!bedwars || !bedwars.name || !kitpvp || !kitpvp.displayName)
-    return undefined;
-
   return {
-    bedwars: {
-      level: bedwars.level,
-      experience: bedwars.exp,
-      coins: bedwars.coins,
-      kills: bedwars.kills,
-      deaths: bedwars.deaths,
-      finalKills: bedwars.final_kills,
-      finalDeaths: bedwars.final_deaths,
-      wins: bedwars.wins,
-      losses: bedwars.played - bedwars.wins,
-      winstreak: bedwars.winstreak,
-      highestWinstreak: bedwars.h_winstreak,
-    },
-    kitpvp: {
-      balance: kitpvp.balance,
-      kills: kitpvp.kills,
-      deaths: kitpvp.deaths,
-      bounty: kitpvp.bounty,
-      highestBounty: kitpvp.topBounty,
-      streak: kitpvp.streak,
-      highestStreak: kitpvp.topstreak,
-    },
+    bedwars:
+      bedwars && bedwars.name
+        ? {
+            level: bedwars.level,
+            experience: bedwars.exp,
+            coins: bedwars.coins,
+            kills: bedwars.kills,
+            deaths: bedwars.deaths,
+            finalKills: bedwars.final_kills,
+            finalDeaths: bedwars.final_deaths,
+            wins: bedwars.wins,
+            losses: bedwars.played - bedwars.wins,
+            winstreak: bedwars.winstreak,
+            highestWinstreak: bedwars.h_winstreak,
+          }
+        : undefined,
+    kitpvp:
+      kitpvp && kitpvp.displayName
+        ? {
+            balance: kitpvp.balance,
+            kills: kitpvp.kills,
+            deaths: kitpvp.deaths,
+            bounty: kitpvp.bounty,
+            highestBounty: kitpvp.topBounty,
+            streak: kitpvp.streak,
+            highestStreak: kitpvp.topstreak,
+          }
+        : undefined,
   };
 }
 
@@ -111,7 +114,7 @@ function getFormattedRank(rawRank: string | undefined): string | undefined {
 }
 
 export async function getPlayerInfo(
-  username: string
+  username: string,
 ): Promise<PlayerInfo | undefined> {
   if (!isUsernameValid(username)) return undefined;
 
