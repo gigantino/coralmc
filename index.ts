48a0193409610bba9ab657a45e21f2ef3d362893
giovanni <git@gigantino.dev>
2025-04-14 19:07:28 +0200

    feat: export types

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
 import getPlayerInfo from "./utils/getPlayerInfo";
 import getPlayerStats from "./utils/getPlayerStats";
 import getBedwarsLeaderboard from "./utils/getBedwarsLeaderboard";
+import type { PlayerInfo, PlayerStats } from "./types/Player";
+import type { LeaderboardEntry } from "./types/Leaderboard";
 
-export { getPlayerInfo, getPlayerStats, getBedwarsLeaderboard };
+export {
+  // Functions
+  getPlayerInfo,
+  getPlayerStats,
+  getBedwarsLeaderboard,
+  // Types
+  PlayerInfo,
+  PlayerStats,
+  LeaderboardEntry,
+};
